60ee07d84eff0e141443bd3f30206e4fb4c89953
Andrew Mette <49996369+GroshfengTheBarbarian@users.noreply.github.com>
2019-05-10 14:50:33 -0500

    Add files via upload

---
+++ AssertAll/Models/AssertStatement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssertAll.Models
+{
+    internal class AssertStatement
+    {
+        internal string TestName { get; set; }
+        internal string File {get;set;}
+        internal int LineNumber {get;set;}
+        internal Action Action {get;set;}
+    }
+}
---
+++ AssertAll/Models/TestInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace AssertAll.Models
+{
+    internal class TestInfo
+    {
+        internal string TestName { get; set; }
+        internal string File { get; set; }
+        internal int LineNumber { get; set; }
+    }
+}
